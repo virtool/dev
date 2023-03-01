24ae25ae19f70f4010379d2fb9a38e7cd8e2a9eb
Reece Hoffmann <reecehoffmann@outlook.com>
2023-03-01 08:35:12 -0800

    chore: update images

--- update/update.py
+++ update/update.py
@@ -21,10 +21,10 @@ yaml.indent(mapping=2, sequence=4, offset=2)
 
 
 YAML_DIRECTORIES = [
-    "../manifests",
-    "../manifests/jobs",
-    "../manifests/tasks",
-    "../manifests/tasks/task_yaml",
+    "manifests",
+    "manifests/jobs",
+    "manifests/tasks",
+    "manifests/tasks/task_yaml",
 ]
 
 
