354043ebf54ec3ff1feae12b829fd39f360103f8
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2024-05-07 09:53:53 -0700

    docs: add RTD config

--- docs/conf.py
+++ docs/conf.py
@@ -6,20 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Virtool Dev'
-copyright = '2024, Ian Boyes, Reece Hoffmann'
-author = 'Ian Boyes, Reece Hoffmann'
+project = "Virtool Dev"
+copyright = "2024, Ian Boyes, Reece Hoffmann"
+author = "Ian Boyes, Reece Hoffmann"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
-html_static_path = ['_static']
+html_theme = "piccolo_theme"
+# html_static_path = ['_static']
