1935da6b26afde99379e81d8a14c15073ec11d13
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2024-05-07 17:01:26 -0700

    docs: deploy docs on readthedocs
    * Configure readthedocs and intersphinx.
    * Fix CSS menu bug.

--- docs/conf.py
+++ docs/conf.py
@@ -6,20 +6,39 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Virtool Dev'
-copyright = '2024, Ian Boyes, Reece Hoffmann'
-author = 'Ian Boyes, Reece Hoffmann'
+project = "Virtool Dev"
+copyright = "2024, Ian Boyes, Reece Hoffmann"
+author = "Ian Boyes, Reece Hoffmann"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.intersphinx",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
-html_static_path = ['_static']
+html_theme = "piccolo_theme"
+# html_static_path = ['_static']
+
+
+# -- Options for intersphinx extension ---------------------------------------
+
+intersphinx_mapping = {
+    "virtool": (
+        "https://virtool-test.readthedocs.io/en/latest/",
+        None,
+    ),
+    "workflow": (
+        "https://virtool-test.readthedocs.io/projects/workflow/en/latest/",
+        None,
+    ),
+    "core": ("https://virtool-test.readthedocs.io/projects/core/en/latest/", None),
+}
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
+# intersphinx_disabled_reftypes = ["*"]
