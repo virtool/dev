e893770825c3532dbf270ebf47ccc0ef0b41b049
Ian Boyes <igboyes@gmail.com>
2025-12-09 10:09:03 -0800

    feat: update init.sh

--- scripts/hosts.sh
+++
@@ -1,8 +0,0 @@
-# Get the IP address of the minikube cluster.
-MINIKUBE_IP=$(minikube ip)
-
-# Delete the existing line for virtool.local in the hosts file.
-sudo sed -i '/virtool.local/d' /etc/hosts
-
-# Add the minikube IP to the hosts file.
-sudo sed -i "/^127\.0\.1\.1/a $MINIKUBE_IP\tvirtool.local" /etc/hosts
